3b7c2e2bd470d365b2828018595478d2024f949a
Ab9ma <abukuma777@users.noreply.github.com>
2023-10-19 04:03:07 +0000

    refactor: JRDBTxtToCSV.py

--- JRDBTxtToCSV.py
+++ JRDBTxtToCSV.py
@@ -35,15 +35,13 @@ class JRDBFileConverter:
         - pd.DataFrame: 変換されたデータフレーム。
         """
         if self.file_type == "BAC":
-            return self.read_and_convert_bac_zed(file_path)
+            return self.read_and_convert_bac(file_path)
         elif self.file_type == "CZA":
             return self.read_and_convert_cza(file_path)
         elif self.file_type == "JOA":
             return self.read_and_convert_joa(file_path)
         elif self.file_type == "KZA":
             return self.read_and_convert_kza(file_path)
-        elif self.file_type == "ZED":
-            return self.read_and_convert_bac_zed(file_path)
         # elif self.file_type == "JOA":
         #     return self.read_and_convert_joa(file_path)
         # elif self.file_type == "JOA":
@@ -56,10 +54,14 @@ class JRDBFileConverter:
         #     return self.read_and_convert_joa(file_path)
         # elif self.file_type == "JOA":
         #     return self.read_and_convert_joa(file_path)
+
+        # TODO: ZEDは保留
+        # elif self.file_type == "ZED":
+        #     return self.read_and_convert_bac(file_path)
         else:
             raise ValueError("Unsupported file type")
 
-    # def read_and_convert_bac_zed(self, file_path):
+    # def read_and_convert_bac(self, file_path):
     #     """
     #     BAC or ZED ファイルを読み込み、データフレームに変換する。
 
@@ -125,7 +127,7 @@ class JRDBFileConverter:
     #     # データリストからデータフレームを作成して返す
     #     return pd.DataFrame(data_list)
 
-    def read_and_convert_bac_zed(self, file_path):
+    def read_and_convert_bac(self, file_path):
         """
         BAC or ZED ファイルを読み込み、データフレームに変換する。
 
@@ -156,31 +158,31 @@ class JRDBFileConverter:
                     "発走時間": byte_str[16:20].decode(detected_encoding).strip(),
                     "距離": byte_str[20:24].decode(detected_encoding).strip(),
                     "芝ダ障害コード": byte_str[24:25].decode(detected_encoding).strip(),
-                    "右左": byte_str[25:26].decode(detected_encoding, errors="ignore").strip(),
-                    "内外": byte_str[26:27].decode(detected_encoding, errors="ignore").strip(),
-                    "種別": byte_str[27:29].decode(detected_encoding, errors="ignore").strip(),
-                    "条件": byte_str[29:31].decode(detected_encoding, errors="ignore").strip(),
-                    "記号": byte_str[31:34].decode(detected_encoding, errors="ignore").strip(),
-                    "重量": byte_str[34:35].decode(detected_encoding, errors="ignore").strip(),
-                    "グレード": byte_str[35:36].decode(detected_encoding, errors="ignore").strip(),
-                    "レース名": byte_str[36:86].decode(detected_encoding, errors="ignore").strip(),
-                    "回数": byte_str[86:94].decode(detected_encoding, errors="ignore").strip(),
-                    "頭数": byte_str[94:96].decode(detected_encoding, errors="ignore").strip(),
-                    "コース": byte_str[96:97].decode(detected_encoding, errors="ignore").strip(),
-                    "開催区分": byte_str[97:98].decode(detected_encoding, errors="ignore").strip(),
-                    "レース名短縮": byte_str[98:106].decode(detected_encoding, errors="ignore").strip(),
-                    "レース名９文字": byte_str[106:124].decode(detected_encoding, errors="ignore").strip(),
-                    "データ区分": byte_str[124:125].decode(detected_encoding, errors="ignore").strip(),
-                    "１着賞金": byte_str[125:130].decode(detected_encoding, errors="ignore").strip(),
-                    "２着賞金": byte_str[130:135].decode(detected_encoding, errors="ignore").strip(),
-                    "３着賞金": byte_str[135:140].decode(detected_encoding, errors="ignore").strip(),
-                    "４着賞金": byte_str[140:145].decode(detected_encoding, errors="ignore").strip(),
-                    "５着賞金": byte_str[145:150].decode(detected_encoding, errors="ignore").strip(),
-                    "１着算入賞金": byte_str[150:155].decode(detected_encoding, errors="ignore").strip(),
-                    "２着算入賞金": byte_str[155:160].decode(detected_encoding, errors="ignore").strip(),
-                    "馬券発売フラグ": byte_str[160:176].decode(detected_encoding, errors="ignore").strip(),
-                    "WIN5フラグ": byte_str[176:177].decode(detected_encoding, errors="ignore").strip(),
-                    "予備": byte_str[177:182].decode(detected_encoding, errors="ignore").strip(),
+                    "右左": byte_str[25:26].decode(detected_encoding).strip(),
+                    "内外": byte_str[26:27].decode(detected_encoding).strip(),
+                    "種別": byte_str[27:29].decode(detected_encoding).strip(),
+                    "条件": byte_str[29:31].decode(detected_encoding).strip(),
+                    "記号": byte_str[31:34].decode(detected_encoding).strip(),
+                    "重量": byte_str[34:35].decode(detected_encoding).strip(),
+                    "グレード": byte_str[35:36].decode(detected_encoding).strip(),
+                    "レース名": byte_str[36:86].decode(detected_encoding).strip(),
+                    "回数": byte_str[86:94].decode(detected_encoding).strip(),
+                    "頭数": byte_str[94:96].decode(detected_encoding).strip(),
+                    "コース": byte_str[96:97].decode(detected_encoding).strip(),
+                    "開催区分": byte_str[97:98].decode(detected_encoding).strip(),
+                    "レース名短縮": byte_str[98:106].decode(detected_encoding).strip(),
+                    "レース名９文字": byte_str[106:124].decode(detected_encoding).strip(),
+                    "データ区分": byte_str[124:125].decode(detected_encoding).strip(),
+                    "１着賞金": byte_str[125:130].decode(detected_encoding).strip(),
+                    "２着賞金": byte_str[130:135].decode(detected_encoding).strip(),
+                    "３着賞金": byte_str[135:140].decode(detected_encoding).strip(),
+                    "４着賞金": byte_str[140:145].decode(detected_encoding).strip(),
+                    "５着賞金": byte_str[145:150].decode(detected_encoding).strip(),
+                    "１着算入賞金": byte_str[150:155].decode(detected_encoding).strip(),
+                    "２着算入賞金": byte_str[155:160].decode(detected_encoding).strip(),
+                    "馬券発売フラグ": byte_str[160:176].decode(detected_encoding).strip(),
+                    "WIN5フラグ": byte_str[176:177].decode(detected_encoding).strip(),
+                    "予備": byte_str[177:182].decode(detected_encoding).strip(),
                 }
                 # スペースをNaNに置換
                 for key, value in data.items():
